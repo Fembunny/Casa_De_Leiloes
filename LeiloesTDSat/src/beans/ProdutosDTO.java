fea28054ba07612964e1e9ca1c3400a08a1c514f
Izabella Chaves <bellacyrne@gmail.com>
2023-10-14 19:22:58 -0300

    Update - atualizando a função SalvarProduos

--- LeiloesTDSat/src/beans/ProdutosDTO.java
+++ LeiloesTDSat/src/beans/ProdutosDTO.java
@@ -1,14 +1,5 @@
 package beans;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author Adm
- */
 public class ProdutosDTO {
     private Integer id;
     private String nome;
--- LeiloesTDSat/src/dao/ProdutosDAO.java
+++ LeiloesTDSat/src/dao/ProdutosDAO.java
@@ -1,36 +1,37 @@
 package dao;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author Adm
- */
-
 import beans.ProdutosDTO;
 import java.sql.PreparedStatement;
 import java.sql.Connection;
 import javax.swing.JOptionPane;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 
 public class ProdutosDAO {
-    
-    Connection conn;
-    PreparedStatement prep;
-    ResultSet resultset;
+    private Connection conn;
+    private PreparedStatement st;
+    private ResultSet rs;
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
     
-    public void cadastrarProduto (ProdutosDTO produto){
-        
-        
-        //conn = new conectaDAO().connectDB();
-        
-        
+    public int cadastrarProduto(ProdutosDTO produto){
+        int status;
+        try{
+            st = conn.prepareStatement("INSERT INTO produtos (nome, valor, status) VALUES (?,?,?)");
+            st.setString(1, produto.getNome());
+            st.setDouble(2,produto.getValor());
+            st.setString(3, produto.getStatus());
+            
+            status = st.executeUpdate();
+            
+            System.out.println("Produto salvo com sucesso");
+            return status;
+        }
+        catch(SQLException ex){
+            System.out.println("Erro ao conectar - " + ex.getMessage());
+            return ex.getErrorCode();
+        }
     }
     
     public ArrayList<ProdutosDTO> listarProdutos(){
