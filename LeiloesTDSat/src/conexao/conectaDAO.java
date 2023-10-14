d5b48d0504a13acab9f44a5f9e77dfe68bff96d1
Izabella Chaves <bellacyrne@gmail.com>
2023-10-13 23:04:19 -0300

    Update - conectaDAO

--- LeiloesTDSat/src/conexao/conectaDAO.java
+++ LeiloesTDSat/src/conexao/conectaDAO.java
@@ -1,35 +1,24 @@
 package conexao;
 
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import javax.swing.JOptionPane;
 
 
-
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author Adm
- */
 public class conectaDAO {
-    
     public Connection connectDB(){
-        Connection conn = null;
-        
         try {
-        
-            conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11?user=root&password=");
+            Class.forName( "com.mysql.cj.jdbc.Driver" );
+            System.out.println( "Driver JDBC carregado" );
             
-        } catch (SQLException erro){
-            JOptionPane.showMessageDialog(null, "Erro ConectaDAO" + erro.getMessage());
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/uc11","root","fembunny9847");
+            System.out.println( "Conexao com o banco de dados estabelecida." );
+            
+            return conn;
+        } 
+        catch (ClassNotFoundException | SQLException sqle ) {
+            System.out.println( "Erro na conexao ao Bando de Dados : " + sqle.getMessage() );
+            return null;
         }
-        return conn;
     }
-    
 }
